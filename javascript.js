226d82afb8aa034358c2cb6a92d1380e76bdb3bd
TJ <thomasjjohnson9311@gmail.com>
2022-05-15 18:23:38 -0500

    Pseudocoding through the logic to plan

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 
 //creates an array containing these strings
-const rpsArray = ["Rock", "Paper", "Scissors", "Shoot"];
+const rpsArray = ["rock", "paper", "scissors"];
 
 // returns a random value in the array
 function computerPlay(){
@@ -8,15 +8,30 @@ function computerPlay(){
     
 }
 
-console.log(computerPlay())
+// declare playerSelection & test against computerSelection
+// TODO: allow playerSelection to be chosen without declaring its value
+const playerSelection = "rock";
+const computerSelection = computerPlay();
 
-
-
-// function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
+    // player win conditions (use if/else):
+    // player rock & computer scissors || player scissors & computer paper ||
+    // player paper & computer rock || && =/= computerSelection THEN "You win!"
+    // ELSE "You lose!"
     
-// }
+    // Remember: create scorekeeping variables that are NOT constant: "let x = 0"
+    // increment +1 to winner after each round
+    // use console to test w/o UI
 
-// const playerSelection = "rock";
-// const computerSelection = computerPlay();
+    // computer win conditions * && =/= playerSelection 
+    
+    
+}
 
-// console.log(playRound);
+
+
+function game(){
+    playRound();
+}
+
+console.log(playRound);
